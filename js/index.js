3cac9d64f70d890ae38357434615478d9845bf7b
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-07-07 22:22:36 +0430

    a

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let nav_nav = document.querySelector('nav');
+let navWrapper_div = document.querySelector('.nav_wrapper');
+let navList_ul = document.querySelector('.nav_list');
+let navItem_li = document.querySelectorAll('.nav_item');
+let navLink_a = document.querySelectorAll('.nav_link');
+let section_section = document.querySelectorAll('.content_section');
+let identifier_div = document.querySelector('.nav_identifier');
+//console.log(identifier_div)
+navItem_li.forEach( thisNavItem_li =>{
+    thisNavItem_li.addEventListener('click' , ()=>{
+        identifier_div.style.left = thisNavItem_li.offsetLeft + 'px';
+    });
+});
+window.addEventListener('scroll', function(){
+    let nowScroll = window.pageYOffset;
+    for (let i = 0; i < section_section.length; i++) {
+        let secStart = section_section[i].offsetTop;
+        let secEnd = section_section[i].scrollHeight + section_section[i].offsetTop;
+        
+        if (nowScroll > secStart && nowScroll < secEnd) {
+            let goTo = section_section[i].dataset.link;
+            navLink_a.forEach(navLink_a => {
+                if (navLink_a.attributes.href.value == goTo ) {
+                    navWrapper_div.scrollLeft = navLink_a.parentElement.offsetLeft; 
+                    identifier_div.style.left = navLink_a.parentElement.offsetLeft + 'px'; 
+                }
+            })
+        } else {
+            continue;
+        }
+    }
+});
+//navWrapper_div.scrollLeft = item.offsetLeft;
+ 
